aac562b83f9242b86acf52271b4f51bd31647738
Henrique <henrique.aracomp@gmail.com>
2021-03-09 15:51:26 -0300

    My answers to challenge 2

--- dom/desafio-02/app.js
+++ dom/desafio-02/app.js
@@ -2,5 +2,13 @@ new Vue({
     el: '#desafio',
     data: {
         valor: ''
+    },
+    methods:{
+        alerta(){
+            alert("Estou te alertando!")
+        },
+        keydown(event){
+            this.valor = event.target.value
+        }
     }
 })
