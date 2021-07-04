f4bf3238e561581bde733af848ef9aa41b1b4299
Henrique <henrique.aracomp@gmail.com>
2021-07-04 14:28:51 -0300

    Heroku host

--- pp/server.js
+++ pp/server.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname + '/dist'));
 
 const server = app.listen(8081, () => {
 
-  const host = server.address().address;
+  const host = process.env.PORT ? '0.0.0.0' : server.address().address;
   const port = process.env.PORT || server.address().port;
 
   console.log('App listening at http://%s:%s', host, port);
