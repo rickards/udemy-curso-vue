fa1cdf1f2d19d54c419d4df1151a23bb7485fd15
Henrique <henrique.aracomp@gmail.com>
2021-07-04 14:22:57 -0300

    Assigns your app a port

--- pp/server.js
+++ pp/server.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + '/dist'));
 const server = app.listen(8081, () => {
 
   const host = server.address().address;
-  const port = server.address().port;
+  const port = process.env.PORT || server.address().port;
 
   console.log('App listening at http://%s:%s', host, port);
 });
