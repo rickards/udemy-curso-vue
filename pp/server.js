6c44d5bb2d8f21d43928aad6e49f62d871395896
Henrique <henrique.aracomp@gmail.com>
2021-07-04 15:00:49 -0300

    Host and Port  server update

--- pp/server.js
+++ pp/server.js
@@ -3,10 +3,13 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-const server = app.listen(8081, () => {
+var server_port = process.env.PORT || 8081;
+var server_host = process.env.PORT ? '0.0.0.0' : undefined;
 
-  const host = process.env.PORT ? '0.0.0.0' : server.address().address;
-  const port = process.env.PORT || server.address().port;
+const server = app.listen(server_port, server_host, () => {
+
+  const host = server.address().address;
+  const port = server.address().port;
 
   console.log('App listening at http://%s:%s', host, port);
 });
