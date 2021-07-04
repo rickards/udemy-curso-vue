7f4ba14ac5883e9f9141589975042e15f014cec8
Henrique <henrique.aracomp@gmail.com>
2021-07-04 13:59:23 -0300

    Heroku integrate

---
+++ pp/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+const server = app.listen(8081, () => {
+
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
+});
