1e750ceef824ff33695f4a24e12b6478931d537c
Henrique <henrique.aracomp@gmail.com>
2022-06-29 19:25:39 -0400

    Show database inline

--- pp/src/pages/Backup.vue
+++ pp/src/pages/Backup.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>
-        <button @click="downloadCSVData">Download</button>
-    </div>
+  <div>
+    <button @click="downloadCSVData">Download</button>
+    <ul>
+    <li v-for="item in databaseArray" :key="item">{{item}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -9,35 +12,38 @@ import database from "@/helpers/db-interface";
 // import JsonCSV from 'vue-json-csv'
 
 export default {
-    name: "Backup",
-    // components: {
-    //     'downloadCsv': JsonCSV
-    // },
-    created(){
-        console.log("print database")
+  name: "Backup",
+  // components: {
+  //     'downloadCsv': JsonCSV
+  // },
+  created() {
+    console.log("print database");
+    database.getExpensesDatabase().then((resp) => {
+      resp.forEach((element) => {
+        this.databaseArray.push(Object.values(element).join(","));
+      });
+    });
+  },
+  data() {
+    return {
+      databaseArray: [],
+    };
+  },
+  methods: {
+    downloadCSVData() {
+      console.log("Downloading...");
+
+      const anchor = document.createElement("a");
+      anchor.href =
+        "data:text/csv;charset=utf-8," +
+        encodeURIComponent(this.databaseArray.join("\n"));
+      anchor.target = "_blank";
+      anchor.download = "backup.csv";
+      anchor.click();
     },
-    methods: {
-        downloadCSVData() {
-            console.log("Downloading...")
-            let csv = 'Put,Column,Titles,Here\n';
-
-            database.getExpensesDatabase().then((resp) => {
-                resp.forEach((element) => {
-                    csv += Object.values(element).join(',');
-                    csv += "\n";
-                });
-
-                const anchor = document.createElement('a');
-                anchor.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv);
-                anchor.target = '_blank';
-                anchor.download = 'backup.csv';
-                anchor.click();
-            });
-        }
-    }
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
