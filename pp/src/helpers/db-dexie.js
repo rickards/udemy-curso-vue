a0471e10ad71c79c332d3d0c28b4d8911ef52bb7
Henrique <henrique.aracomp@gmail.com>
2022-07-26 20:24:17 -0400

    Remove populate db

--- pp/src/helpers/db-dexie.js
+++ pp/src/helpers/db-dexie.js
@@ -11,47 +11,47 @@ db.version(0.2).stores({
   qaAnalysis: "++id",
 });
 
-db.on("populate", function() {
-  // Init your DB with some default expenses:
-  let i;
-  for (i=0; i<20; i+=10){
-    db.expenses.add({id: i+0, date: getDateBetween1Year(), type: "Despesa",name: "Mercado", value: getRandomInt(100, 100000)});
-    db.expenses.add({id: i+1, date: getDateBetween1Year(), type: "Despesa",name: "Lanche", value: getRandomInt(100, 10000)});
-    db.expenses.add({id: i+2, date: getDateBetween1Year(), type: "Despesa",name: "Farmácia", value: getRandomInt(100, 10000)});
-    db.expenses.add({id: i+3, date: getDateBetween1Year(), type: "Despesa",name: "Aluguel", value: getRandomInt(30000, 500000)});
-    db.expenses.add({id: i+4, date: getDateBetween1Year(), type: "Despesa",name: "Energia", value: getRandomInt(8000, 40000)});
+// db.on("populate", function() {
+//   // Init your DB with some default expenses:
+//   let i;
+//   for (i=0; i<20; i+=10){
+//     db.expenses.add({id: i+0, date: getDateBetween1Year(), type: "Despesa",name: "Mercado", value: getRandomInt(100, 100000)});
+//     db.expenses.add({id: i+1, date: getDateBetween1Year(), type: "Despesa",name: "Lanche", value: getRandomInt(100, 10000)});
+//     db.expenses.add({id: i+2, date: getDateBetween1Year(), type: "Despesa",name: "Farmácia", value: getRandomInt(100, 10000)});
+//     db.expenses.add({id: i+3, date: getDateBetween1Year(), type: "Despesa",name: "Aluguel", value: getRandomInt(30000, 500000)});
+//     db.expenses.add({id: i+4, date: getDateBetween1Year(), type: "Despesa",name: "Energia", value: getRandomInt(8000, 40000)});
 
-    db.expenses.add({id: i+5, date: getDateBetween1Year(), type: "Receita",name: "Faturamento", value: getRandomInt(1000000, 5000000)});
-    db.expenses.add({id: i+6, date: getDateBetween1Year(), type: "Receita",name: "Alugueis", value: getRandomInt(100, 40000)});
-    db.expenses.add({id: i+7, date: getDateBetween1Year(), type: "Bem_gasto",name: "Carro", value: getRandomInt(1800000, 4800000)});
-    db.expenses.add({id: i+8, date: getDateBetween1Year(), type: "Bem_ganho",name: "Macbook", value: getRandomInt(500000, 1200000)});
-    db.expenses.add({id: i+9, date: getDateBetween1Year(), type: "Receita",name: "Vendas", value: getRandomInt(8000, 40000)});
-  }
-  db.expenses.add({id: i+1, date: getDateBetween1Year(), type: "Investimento", name: "VRTA11", value: 100 * 105.99 * 200, qtde: 200 });
-  db.expenses.add({id: i+2, date: getDateBetween1Year(), type: "Investimento", name: "RBRP11", value: 100 * 92.5 * 300, qtde: 300 });
-  db.expenses.add({id: i+3, date: getDateBetween1Year(), type: "Investimento", name: "ABCB4", value: 100 * 14.75 * 1800, qtde: 1800 });
-  db.expenses.add({id: i+4, date: getDateBetween1Year(), type: "Investimento", name: "VRTA11", value: 100 * 108.99 * 500, qtde: 500 });
+//     db.expenses.add({id: i+5, date: getDateBetween1Year(), type: "Receita",name: "Faturamento", value: getRandomInt(1000000, 5000000)});
+//     db.expenses.add({id: i+6, date: getDateBetween1Year(), type: "Receita",name: "Alugueis", value: getRandomInt(100, 40000)});
+//     db.expenses.add({id: i+7, date: getDateBetween1Year(), type: "Bem_gasto",name: "Carro", value: getRandomInt(1800000, 4800000)});
+//     db.expenses.add({id: i+8, date: getDateBetween1Year(), type: "Bem_ganho",name: "Macbook", value: getRandomInt(500000, 1200000)});
+//     db.expenses.add({id: i+9, date: getDateBetween1Year(), type: "Receita",name: "Vendas", value: getRandomInt(8000, 40000)});
+//   }
+//   db.expenses.add({id: i+1, date: getDateBetween1Year(), type: "Investimento", name: "VRTA11", value: 100 * 105.99 * 200, qtde: 200 });
+//   db.expenses.add({id: i+2, date: getDateBetween1Year(), type: "Investimento", name: "RBRP11", value: 100 * 92.5 * 300, qtde: 300 });
+//   db.expenses.add({id: i+3, date: getDateBetween1Year(), type: "Investimento", name: "ABCB4", value: 100 * 14.75 * 1800, qtde: 1800 });
+//   db.expenses.add({id: i+4, date: getDateBetween1Year(), type: "Investimento", name: "VRTA11", value: 100 * 108.99 * 500, qtde: 500 });
 
-  // Init your DB with some default expensesStocks:
-  db.expensesStocks.add({regex: "Despesas=.*?" });
-  db.expensesStocks.add({regex: "Lanche" });
-  db.expensesStocks.add({regex: "Farmácia" });
-  db.expensesStocks.add({regex: ".*?" });
-  db.expensesStocks.add({regex: "Aluguel" });
-  db.expensesStocks.add({regex: "Extra" });
-});
+//   // Init your DB with some default expensesStocks:
+//   db.expensesStocks.add({regex: "Despesas=.*?" });
+//   db.expensesStocks.add({regex: "Lanche" });
+//   db.expensesStocks.add({regex: "Farmácia" });
+//   db.expensesStocks.add({regex: ".*?" });
+//   db.expensesStocks.add({regex: "Aluguel" });
+//   db.expensesStocks.add({regex: "Extra" });
+// });
 
-function getDateBetween1Year(){
-  const today = new Date()
-  const diff = getRandomInt(100000000, 100000000*365)
-  return new Date(today-diff).toISOString().slice(0, 10)
-}
+// function getDateBetween1Year(){
+//   const today = new Date()
+//   const diff = getRandomInt(100000000, 100000000*365)
+//   return new Date(today-diff).toISOString().slice(0, 10)
+// }
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+// function getRandomInt(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
 
 function getExpenses() {
   return db.expenses.toArray();
