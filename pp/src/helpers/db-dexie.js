77864379122d1d05931b2f6c168cc92a863c3385
Henrique <henrique.aracomp@gmail.com>
2022-08-02 17:57:01 -0400

    Fix id when use app android

--- pp/src/helpers/db-dexie.js
+++ pp/src/helpers/db-dexie.js
@@ -15,19 +15,20 @@ db.version(0.2).stores({
 
 db.on("populate", function() {
   if(firebase.FIREBASE_ENVIROMENT){
-    let stringDatabase = firebase.DB.initialState()
+    let stringDatabase = firebase.DB.data()
     // let stringDatabase = "{expensesStocks={-N8GgJc0JFx8fCuaMmyv={regex=Lanche}, -N8GgJZodCwEpYDPjJFu={regex=Despesas=.*?}, -N8GgJfN6z71Jtr7nU5p={regex=Aluguel=Aluguel}}, expense={-N8GiKlkMYg-E146ohPm={date=2020-02-15, name=Natação, value=1500, type=Despesa, qtde=0}, -N8GiLbYbeF-NhW_m4oe={date=2020-02-19, name=Ônibus, value=365, type=Despesa, qtde=0}, -N8GiKrWs3IBLfTjrQvj={date=2020-02-17, name=Enxoval pf, value=12000, type=Despesa, qtde=0}, -N8GiKwQNN61TSCwbrLH={date=2020-02-17, name=Farmácia, value=6673, type=Despesa, qtde=0}, -N8GiL0TY29wXDkBtS7q={date=2020-02-17, name=Academia, value=6000, type=Despesa, qtde=0}, -N8GiLWDw_aO9to86FLO={date=2020-02-19, name=Almoço, value=1590, type=Despesa, qtde=0}, -N8GiLLoW7e07SB3ODV9={date=2020-02-19, name=Van, value=2300, type=Despesa, qtde=0}, -N8GiLmmzW8iSQqxgRD-={date=2020-02-21, name=Natação, value=1525, type=Despesa, qtde=0}, -N8GiLBfBQQGwytcg6Db={date=2020-02-18, name=Mercado, value=600, type=Despesa, qtde=0}, -N8GiLgSCiKONqwc2duc={date=2020-02-19, name=Ônibus, value=365, type=Despesa, qtde=0}, -N8GiLre0IqSYOaUOeTr={date=2020-02-21, name=Cabelo, value=1000, type=Despesa, qtde=0}, -N8GiL6o_BGAXKl3RYdG={date=2020-02-17, name=Almoço, value=1889, type=Despesa, qtde=0}, -N8GiLGkb6CmfOii_w-r={date=2020-02-18, name=Mercado, value=500, type=Despesa, qtde=0}, -N8GiLRR6X18toqcICi9={date=2020-02-19, name=Van, value=2300, type=Despesa, qtde=0}}}"
 
     let re_expense_table = new RegExp("expense={(.*?)}}");
     let stringExpenses = (re_expense_table.exec(stringDatabase)[1])+"}";
     
-    const array = [...stringExpenses.matchAll("{(.*?)}")];
+    const array = [...stringExpenses.matchAll(/(\S+)={(.*?)}/g)];
     console.log("matchs", array)
     array.forEach((match)=>{
       let expense = match[0].replaceAll(/(\d{4}-\d{2}-\d{2})/ig, "'$1'")
   
       if(!expense.match(/del=1[,}]/i)){
         db.expenses.add({
+          id: match[1],
           date: expense.match(/(\d{4}-\d{2}-\d{2})/i)[1],
           type: expense.match(/type=(.*?)[,}]/i)[1],
           name: expense.match(/name=(.*?)[,}]/i)[1], 
@@ -40,13 +41,17 @@ db.on("populate", function() {
     let re_expenseStock_table = new RegExp("expensesStocks={(.*?)}}");
     let stringExpenseStock = (re_expenseStock_table.exec(stringDatabase)[1])+"}";
     
-    const arrayExpenseStock = [...stringExpenseStock.matchAll("{(.*?)}")];
+    const arrayExpenseStock = [...stringExpenseStock.matchAll(/(\S+)={(.*?)}/g)];
     arrayExpenseStock.forEach((match)=>{
       let expenseStock = match[0]
   
-      db.expensesStocks.add({
-        regex: expenseStock.match(/regex=(.*?)[,}]/i)[1]
-      });
+      if(!expenseStock.match(/del=1[,}]/i)){
+        db.expensesStocks.add({
+          id: match[1],
+          regex: expenseStock.match(/regex=(.*?)[,}]/i)[1]
+        });
+      }
+      
     })
   }
 });
