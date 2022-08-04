c693b1571ea18c9c1ad1103acb2388c83adb46ab
Henrique <henrique.aracomp@gmail.com>
2022-08-03 22:59:39 -0400

    Enable importData page

--- pp/src/App.vue
+++ pp/src/App.vue
@@ -17,14 +17,14 @@
       <div class="button-card" @click="page = 'ExpenseExplorer'">
         Explorador de Despesas
       </div>
+      <div class="button-card" @click="page = 'ImportData'">
+        ImportData
+      </div>
 
     </div>
     <div class="grid" v-if="androidEnv">
       <div class="button-card" @click="page = 'Investiments'">
         Investimentos
-      </div>
-      <div class="button-card" @click="page = 'ImportData'">
-        ImportData
       </div>
       <div class="button-card" @click="page = 'Analytics'">
         Análise de Cotações
